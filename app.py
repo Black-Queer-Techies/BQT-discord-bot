0eb83eddf7f61b7e5037e99ba0b939cc233dd437
Damien Burks <dburksgtr@gmail.com>
2022-01-05 17:25:04 -0600

    finalizing changes

--- app.py
+++ app.py
@@ -4,14 +4,9 @@ import discord
 
 from clients.main_bot_client import MainBotClient
 
-# Code for discord channel
-
-#token = os.getenv("DISCORD_TOKEN")
-#my_guild = os.getenv("DISCORD_GUILD")
-
 def run_bot():
-    token = "OTI4MDI0MjI2ODc3ODI1MTM0.YdSv6Q.VRTp6YN4tpwUOrR2vGzEtfeNNGQ"
-    my_guild = "927996396823515206"    
+    token = os.getenv("DISCORD_TOKEN")
+    my_guild = os.getenv("DISCORD_GUILD")
     
     intents = discord.Intents.default()
     my_client = discord.Client(intents=intents)
