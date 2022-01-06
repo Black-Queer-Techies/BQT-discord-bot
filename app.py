be32c87062d33adba98e3212d21ac0d3bfd9bfa2
Damien Burks <damien@Damiens-MBP.attlocal.net>
2022-01-05 22:25:40 -0600

    adding black and formatting code

--- app.py
+++ app.py
@@ -4,19 +4,18 @@ import discord
 
 from clients.main_bot_client import MainBotClient
 
+
 def run_bot():
     token = os.getenv("DISCORD_TOKEN")
     my_guild = os.getenv("DISCORD_GUILD")
-    
-    
+
     intents = discord.Intents.default()
     my_client = discord.Client(intents=intents)
 
-    bot = MainBotClient(
-        command_prefix='$'
-    )
+    bot = MainBotClient(command_prefix="$")
 
     bot.run(token)
+
 
 if __name__ == "__main__":
     run_bot()
