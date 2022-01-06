c8422b97ad846613a2493783c2526e794fcb3634
Damien Burks <dburksgtr@gmail.com>
2022-01-06 12:08:59 -0600

    adding only greetings cog for now

--- app.py
+++ app.py
@@ -4,10 +4,11 @@ import discord
 
 from client.main_bot_client import MainBotClient
 from cogs.tech_news_cog import TechNewsCog
-
+from cogs.greetings import Greetings
 
 def start():
-    token = os.getenv("DISCORD_TOKEN")
+    #token = os.getenv("DISCORD_TOKEN")
+    token = "OTI4MDI0MjI2ODc3ODI1MTM0.YdSv6Q.novm4LxwzWZ8h3LbCzTsLPxf-T8"
 
     intents = discord.Intents.default()
     intents.members = True
@@ -15,7 +16,8 @@ def start():
     bot = MainBotClient(command_prefix="$", intents=intents)
 
     # Adding cogs
-    bot.add_cog(TechNewsCog(bot))
+    # bot.add_cog(TechNewsCog(bot))
+    bot.add_cog(Greetings(bot))
 
     bot.run(token)
 
--- cogs/greetings.py
+++ cogs/greetings.py
@@ -5,12 +5,7 @@ class Greetings(commands.Cog, name="Greetings module"):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command(name="hey")
-    async def adhoc_play(self, client):
-        await client.send(f"Hey {client.author.name}")
-
     @commands.Cog.listener()
     async def on_member_join(self, member):
-        channel = member.guild.system_channel
-        if channel is not None:
-            await channel.send(f"A wild {member.mention} has appeared!")
+        channel = self.bot.get_channel(928000257973964810)
+        await channel.send(f"Welcome to Black Queer Techies {member.mention}!")
