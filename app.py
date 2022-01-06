405bc59b7a60e1c66994388fdcf52ad83aaf4ff6
Damien Burks <damien@Damiens-MBP.attlocal.net>
2022-01-05 23:04:29 -0600

    publishing latest code: adding cogs for more bot functionality

--- app.py
+++ app.py
@@ -7,13 +7,8 @@ from clients.main_bot_client import MainBotClient
 
 def run_bot():
     token = os.getenv("DISCORD_TOKEN")
-    my_guild = os.getenv("DISCORD_GUILD")
-
-    intents = discord.Intents.default()
-    my_client = discord.Client(intents=intents)
 
     bot = MainBotClient(command_prefix="$")
-
     bot.run(token)
 
 
---
+++ cogs/tech_news_cog.py
@@ -0,0 +1,22 @@
+import discord
+
+DISCORD_CHANNEL_ID = 928409579820290069
+
+
+class TechNewsCog(discord.ext.commands.Cog, name="Tech News module"):
+    """
+    This class contains processes that obtain
+    the latest news articles in tech and publishes them
+    to the daily-tech-news channel.
+    """
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    def get_news_article(self):
+        return "Testing the works..."
+
+    @discord.ext.commands.command(name="publish-article")
+    async def publish_news_article(self, client):
+        channel = client.get_channel(DISCORD_CHANNEL_ID)
+        await channel.send(self.get_news_article())
